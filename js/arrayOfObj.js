4e96b646ab2623844937c16d684477fede04ca93
MariusAskelovic <marashz@gmail.com>
2023-07-19 09:35:32 +0300

    youngWomen fn works

--- js/arrayOfObj.js
+++ js/arrayOfObj.js
@@ -71,4 +71,18 @@ const people = [
 
 console.table(people);
 
-printMen(people);
+// printMen(people);
+
+// 2. Naudojant Array.prototype.filter grazinti visas moteris jaunesnes nei 35 metai.
+function youngWomen(arr) {
+  //   const atrinktiObj = people
+  //     .filter((personObj) => personObj.sex === 'female')
+  //     .filter((personObj) => personObj.age < 35);
+  const atrinktiObj = arr.filter(
+    (personObj) => personObj.sex === 'female' && personObj.age < 35
+  );
+  console.table(atrinktiObj);
+  return atrinktiObj;
+}
+const moterys = youngWomen(people);
+console.log('moterys ===', moterys.length);
