a063f5bc933a9b310dd0cad0de3fe86892d256e9
MariusAskelovic <marashz@gmail.com>
2023-07-19 11:35:21 +0300

    add hideModal

--- js/modal.js
+++ js/modal.js
@@ -5,13 +5,20 @@ console.log('modal.js file was loaded');
 const els = {
   showBtn: document.getElementById('show'),
   modalEl: document.getElementById('modal'),
+  cancelBtn: document.getElementById('cancel'),
 };
 
 // event listeners
-
+els.showBtn.addEventListener('click', showModal);
+els.cancelBtn.addEventListener('click', hideModal);
 // functions
 
 // showModal() - nuimti d-none nuo modalo
 function showModal() {
   els.modalEl.classList.remove('d-none');
+}
+
+// hideModal()
+function hideModal() {
+  els.modalEl.classList.add('d-none');
 }
